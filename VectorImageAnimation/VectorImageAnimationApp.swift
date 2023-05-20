71239d19fcdc8f5fe67ae52bd9a07b6adbbc9047
Asadbek <asadbekn443@gmail.com>
2023-05-20 12:18:04 -0400

    Initial Commit

---
+++ VectorImageAnimation/VectorImageAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  VectorImageAnimationApp.swift
+//  VectorImageAnimation
+//
+//  Created by Asadbek Nematov on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct VectorImageAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
